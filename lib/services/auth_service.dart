c3565adafe49f60046dc254ab45a8a6db96ded70
winfred <winfred242002@gmail.com>
2022-07-22 19:29:54 +0000

    :alien: Log In Anonymous

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -3,19 +3,35 @@
 
 import 'package:firebase_auth/firebase_auth.dart';
 
-
-
 class AuthenticationService{
+
 
 // First create an Instance of the class FirebaseAuth
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
 // A Function to handle Anonymous Sign In
   Future signInAnonymous() async {
-    UserCredential results = await  _auth.signInAnonymously();
-    UserCredential user = results;
-    print(user);
-    return results.user;
+    try {
+      UserCredential results = await  _auth.signInAnonymously();
+      UserCredential user = results;
+      print(user);
+      return results.user;
+    } on Exception catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  Future signInWithEmailAndPassword(String email, String password) async{
+    try {
+      UserCredential results = await _auth.signInWithEmailAndPassword(email: email, password: password);
+      UserCredential user = results;
+      print(user);
+      return results.user;
+    } on Exception catch (e) {
+      print(e.toString());
+      return null;
+    }
   }
 
 
